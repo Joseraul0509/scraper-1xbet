68f502dd98cc3ff3477de9b4ceff05962077556e
Joseraul0509 <joseraulsubit@gmail.com>
2025-04-08 08:40:21 -0400

    Update install_playwright.sh

--- install_playwright.sh
+++ install_playwright.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Crear carpeta de navegadores en /tmp/playwright
-mkdir -p /tmp/playwright
-
-# Descargar los navegadores en esa carpeta
-PLAYWRIGHT_BROWSERS_PATH=/tmp/playwright npx playwright install chromium
+# Descargar únicamente Chromium
+npx playwright install chromium
